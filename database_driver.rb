ac36ad1c92e97c45eeb4a6d46f3ced4488f8a781
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-01-07 20:05:53 -0800

    comment and some refactor

--- database_driver.rb
+++ database_driver.rb
@@ -1,10 +1,24 @@
-
 class DatabaseDriver
   def initialize(database, user, password)
     @database = database
     @user = user
     @password = password
     @connected = false
+  end
+
+  def self.open(database, user, password)
+    driver = self.new(database, user, password)
+    driver.connect
+# if a block is not given it will return the object itself
+
+    return driver unless block_given?
+
+    begin
+      yield(driver)
+# the method disconnet is ran always, even when an exception is raised
+    ensure
+      driver.disconnect
+    end
   end
 
   def connect
@@ -18,39 +32,48 @@ class DatabaseDriver
     puts "Disconnected."
   end
 
+  def begin_transaction
+    puts "Beginning transaction..."
+  end
+
+  def commit_transaction
+    puts "Committed transaction."
+  end
+
+  def rollback_transaction
+    puts "Rolled back transaction!"
+  end
+
   def execute(sql)
     raise "Not connected!" unless @connected
     puts "Executing #{sql}..."
-    # executes SQL
   end
 
-  def self.open(database, user, password)
-    databasedriver = self.new(database, user, password)
-    databasedriver.connect
-
-    return databasedriver unless block_given?
-
-    begin
-      yield(databasedriver)
-    ensure
-      databasedriver.disconnect
-    end
+  def transactionally
+    begin_transaction
+    yield
+    commit_transaction
+  rescue Exception => e
+    rollback_transaction
   end
-
 end
 
 DatabaseDriver.open("my_database", "admin", "secret") do |driver|
-  driver.execute("SELECT * FROM ORDERS")
+# it runs the open class method
+  driver.transactionally do
+    driver.execute("UPDATE ORDERS SET status='completed'")
+    driver.execute("DELETE * FROM SHIPPING_QUEUE")
+  end
+
+  # not run in a transaction
+  driver.execute("SELECT * FROM ORddDERS")
   driver.execute("SELECT * FROM USERS")
 end
 
 DatabaseDriver.open("my_database", "admin", "secret") do |driver|
-  driver.execute("DELETE * FROM ORDERS")
-  raise "Boom!"
-  driver.execute("DELETE * FROM USERS")
+  driver.transactionally do
+    driver.execute("UPDATE ORDERS SET status='completed'")
+    raise "Boom!"
+    driver.execute("DELETE * FROM SHIPPING_QUEUE")
+  end
 end
-
-driver = DatabaseDriver.open("my_database", "admin", "secret")
-driver.execute("SELECT * FROM ORDERS")
-driver.execute("SELECT * FROM USERS")
-driver.disconnect
