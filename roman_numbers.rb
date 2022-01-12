d6856f67132c1f6959bd019f98e0d3415001634c
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-01-11 19:24:27 -0800

    roman translator using hash

---
+++ roman_numbers.rb
@@ -0,0 +1,35 @@
+ROMAN = {
+  "I" => 1,
+  "IV" => 4,
+  "V" => 5,
+  "IX" => 9,
+  "X" => 10,
+  "XL" => 40,
+  "L" => 50,
+  "XC" => 90,
+  "C" => 100,
+  "CD" => 400,
+  "D" => 500,
+  "CM" => 900,
+  "M" => 1000
+}
+
+def roman_to_integer(roman_string)
+  number = 0
+  string = roman_string
+  until string.length == 0
+    last_two_characters = p string.slice(-2, 2)
+    if ROMAN.key?(last_two_characters)
+      number += ROMAN[last_two_characters]
+      string.chop!
+    else
+      number += p ROMAN[string.slice(-1)]
+    end
+    string.chop!
+  end
+  number
+end
+
+
+
+puts roman_to_integer("XICDML")
